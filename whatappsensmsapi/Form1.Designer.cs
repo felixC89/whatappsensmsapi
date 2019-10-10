146ba72172c19e2396619744b79f29d307e4ed6c
felixC89 <39625792+felixC89@users.noreply.github.com>
2019-10-09 19:56:05 -0600

    primer commit envio de datos a github

---
+++ whatappsensmsapi/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace whatappsensmsapi
+{
+    partial class whatsappsender
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbmensaje = new System.Windows.Forms.TextBox();
+            this.btnenviar = new System.Windows.Forms.Button();
+            this.rb1 = new System.Windows.Forms.RadioButton();
+            this.rb2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label1.Location = new System.Drawing.Point(25, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mi mensaje a whatsapp";
+            // 
+            // tbmensaje
+            // 
+            this.tbmensaje.Location = new System.Drawing.Point(12, 51);
+            this.tbmensaje.Multiline = true;
+            this.tbmensaje.Name = "tbmensaje";
+            this.tbmensaje.Size = new System.Drawing.Size(260, 95);
+            this.tbmensaje.TabIndex = 1;
+            // 
+            // btnenviar
+            // 
+            this.btnenviar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnenviar.Location = new System.Drawing.Point(46, 152);
+            this.btnenviar.Name = "btnenviar";
+            this.btnenviar.Size = new System.Drawing.Size(200, 23);
+            this.btnenviar.TabIndex = 2;
+            this.btnenviar.Text = "Enviar Mensaje a Telefono";
+            this.btnenviar.UseVisualStyleBackColor = true;
+            this.btnenviar.Click += new System.EventHandler(this.btnenviar_Click);
+            // 
+            // rb1
+            // 
+            this.rb1.AutoSize = true;
+            this.rb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rb1.ForeColor = System.Drawing.Color.Red;
+            this.rb1.Location = new System.Drawing.Point(6, 19);
+            this.rb1.Name = "rb1";
+            this.rb1.Size = new System.Drawing.Size(57, 17);
+            this.rb1.TabIndex = 3;
+            this.rb1.TabStop = true;
+            this.rb1.Text = "Belen";
+            this.rb1.UseVisualStyleBackColor = true;
+            // 
+            // rb2
+            // 
+            this.rb2.AutoSize = true;
+            this.rb2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rb2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.rb2.Location = new System.Drawing.Point(6, 42);
+            this.rb2.Name = "rb2";
+            this.rb2.Size = new System.Drawing.Size(68, 17);
+            this.rb2.TabIndex = 4;
+            this.rb2.TabStop = true;
+            this.rb2.Text = "Antonio";
+            this.rb2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rb1);
+            this.groupBox1.Controls.Add(this.rb2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 191);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(127, 67);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Destinatarios";
+            // 
+            // whatsappsender
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnenviar);
+            this.Controls.Add(this.tbmensaje);
+            this.Controls.Add(this.label1);
+            this.Name = "whatsappsender";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WhatsAppSender";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbmensaje;
+        private System.Windows.Forms.Button btnenviar;
+        private System.Windows.Forms.RadioButton rb1;
+        private System.Windows.Forms.RadioButton rb2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ whatappsensmsapi/Form1.cs
@@ -0,0 +1,78 @@
+﻿//link to site api: https://niceapi.net  ********************ESTA APP ENVIA MENSAJES DE WHASTAPP A LOS NUMEROS REGISTRADOS EN ESTA CUENTA: felix2019  
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
+using System.IO;
+
+namespace whatappsensmsapi
+{
+    public partial class whatsappsender : Form
+    {
+        
+        public whatsappsender()
+        {
+            InitializeComponent();
+        }
+
+        private void btnenviar_Click(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+          
+            string yourid = "HauDGis1ukC6ZPDwUvrfgmZlbGl4X2RvdF9jYWxkZXJvbl9kb3RfbmljX2F0X2dtYWlsX2RvdF9jb20=";
+            string yourmobile = "";
+            string mensaje = tbmensaje.Text;
+
+            if (rb1.Checked)
+            {
+                yourmobile = "+50557421576";
+            }
+            else if (rb2.Checked)
+            {
+                yourmobile = "+50588299992";
+            }
+            else 
+            {
+                MessageBox.Show("No se ha seleccionado ningun numero");
+                this.Cursor = Cursors.Default;
+                Application.Exit();
+            }
+
+            try
+            {
+                string url = "https://NiceApi.net/API";
+                HttpWebRequest request =(HttpWebRequest)WebRequest.Create(url);
+                request.Method="POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.Headers.Add("X-APIId",yourid);
+                request.Headers.Add("X-APIMobile",yourmobile);
+
+                using(StreamWriter StreamOut = new StreamWriter(request.GetRequestStream()))
+                {
+                    StreamOut.Write(mensaje);
+                }
+                using (StreamReader StreamIn = new StreamReader(request.GetResponse().GetResponseStream())) 
+                {
+                    Console.WriteLine(StreamIn.ReadToEnd());
+                }
+
+                MessageBox.Show("El mensaje se ha enviado");
+                tbmensaje.Text = "";
+                this.Cursor = Cursors.Default;
+            }
+            catch (SystemException ex)
+            {
+
+                Console.WriteLine(ex.Message);//throw;
+            }
+
+        }
+    }
+}
